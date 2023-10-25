9f3f7a292b73543b44fd7282b2584ba42734088b
Badeeson Yongyuthmeechai <badeeson@Badeesons-MacBook-Air.local>
2023-10-26 02:41:35 +0700

    split content section component

--- src/components/Athlets/Carousel/index.tsx
+++ src/components/Athlets/Carousel/index.tsx
@@ -1,53 +1,9 @@
 import React, { useState } from 'react';
 import { Carousel } from 'antd';
-import { Typography } from 'antd';
-import '../index.css';
-
-const { Title, Paragraph } = Typography;
-
-interface carouselContentProps {
-  sequence: string;
-  label: string;
-  content: string;
-  type?: string;
-}
-
-const CarouselContent = (props: carouselContentProps) => {
-  const { sequence, label, content, type } = props;
-  return (
-    <Typography>
-      <Title
-        level={2}
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          color: '#C2C2C2',
-          fontWeight: 400
-        }}
-      >
-        <span
-          style={{
-            color: type === 'basketball' ? '#8F6BE8' : '#000000',
-            fontSize: '0.5em',
-            textDecoration: 'underline',
-            textDecorationThickness: 4,
-            textDecorationColor: sequence === '03' ? '#C2C2C2' : '#603EBE',
-            marginRight: 12
-          }}
-        >
-          {sequence}
-        </span>
-        {label}
-      </Title>
-      <Paragraph style={{ color: sequence === '03' ? '#FFFFFF' : '#000000' }}>
-        {content}
-      </Paragraph>
-    </Typography>
-  );
-}
+import ContentSection, { ContentSectionProps } from '../ContentSection';
 
 interface CarouselCustomProps {
-  data: Array<carouselContentProps>;
+  data: Array<ContentSectionProps>;
   type: string;
 }
 
@@ -68,7 +24,7 @@ const CarouselCustom = (props: CarouselCustomProps) => {
     >
       {data.map((item) => {
         return (
-          <CarouselContent
+          <ContentSection
             {...item}
             type={type}
             key={type + item.sequence}
---
+++ src/components/Athlets/ContentSection/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Typography } from 'antd';
+
+const { Title, Paragraph } = Typography;
+
+export interface ContentSectionProps {
+  sequence: string;
+  label: string;
+  content: string;
+  type?: string;
+}
+
+const ContentSection = (props: ContentSectionProps) => {
+  const { sequence, label, content, type } = props;
+  return (
+    <Typography>
+      <Title
+        level={2}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          color: '#C2C2C2',
+          fontWeight: 400
+        }}
+      >
+        <span
+          style={{
+            color: type === 'basketball' ? '#8F6BE8' : '#000000',
+            fontSize: '0.5em',
+            textDecoration: 'underline',
+            textDecorationThickness: 4,
+            textDecorationColor: sequence === '03' ? '#C2C2C2' : '#603EBE',
+            marginRight: 12
+          }}
+        >
+          {sequence}
+        </span>
+        {label}
+      </Title>
+      <Paragraph style={{ color: sequence === '03' ? '#FFFFFF' : '#000000' }}>
+        {content}
+      </Paragraph>
+    </Typography>
+  );
+}
+
+export default ContentSection;
